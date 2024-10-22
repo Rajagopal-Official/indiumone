6e9fa2375ec32814b5dd97414074de372154ea9b
Safrin1302 <safrin.g@indiumsoft.com>
2024-10-22 14:19:08 +0530

    Added home page and its stylings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HelloComponent } from './hello/hello.component';
 import { AuthComponent } from './auth/auth.component';
 import { MsalGuard } from '@azure/msal-angular';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path:'',component:AuthComponent},
-    { path: 'hello', component: HelloComponent },
-    {path:'**',redirectTo:''}
+  { path: '', component: AuthComponent },
+  { path: 'hello', component: HelloComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '' },
 ];
